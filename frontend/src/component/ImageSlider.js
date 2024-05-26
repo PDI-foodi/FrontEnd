3454289865064267707972cbf3c05338045423fe
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-05-26 22:04:28 +0900

    ✨feat: 이미지 슬라이더 추가

--- frontend/src/component/ImageSlider.js
+++ frontend/src/component/ImageSlider.js
@@ -2,19 +2,20 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./ImageSlider.css";
+import Card from "./Card.js";
 
 const ImageSlider = ({ restaurants }) => {
   const settings = {
     dots: true,
     infinite: true,
     speed: 1000,
-    slidesToShow: 5,
+    centerMode: true,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    centerMode: true,
-    variableWidth: true,
-    centerPadding: "1",
+    centerPadding: "1px",
     responsive: [
       {
         breakpoint: 1200,
@@ -35,12 +36,19 @@ const ImageSlider = ({ restaurants }) => {
     <div className="slider-container">
       <Slider {...settings}>
         {restaurants.map((restaurant, index) => (
-          <div>
-            <img src={restaurant.imglink} alt={`Slide ${index + 1}`} />
+          <div key={index}>
+            <Card
+              img={restaurant.imglink}
+              name={restaurant.name}
+              rate={restaurant.rate}
+              rank={`${index + 1}등`}
+              category={restaurant.category}
+            />
           </div>
         ))}
       </Slider>
     </div>
   );
 };
+
 export default ImageSlider;
