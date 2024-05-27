f2ddee31803dd80c32e7f67ce7a0dac198da2993
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-05-27 10:59:01 +0900

    ✨ feat : 백엔드와 연동 완료

--- frontend/src/routes/main/detail/detailPage.jsx
+++ frontend/src/routes/main/detail/detailPage.jsx
@@ -1,14 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import HeaderPage from "../../../components/navbar/HeaderPage";
 import "./detailPage.css";
 import axios from "axios";
+import { useParams } from "react-router-dom";
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InlzeTA2MDUzIiwibmlja25hbWUiOiLsnbTsnqzsnbgiLCJpYXQiOjE3MTY3NzQ1MjUsImV4cCI6MTcxNzAzMzcyNX0.2XG3o1SmC8yBptHP3SBZWlPTQ_w_wupaaHBTgvBq-GU";
 
 const DetailPage = () => {
+  const params = useParams();
   useEffect(() => {
-    const fetchData = async () => {
-      axios.get("");
+    const test = async () => {
+      const res = await axios.get(`/detail/${params.detailId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log(res);
     };
-  });
+    test();
+  }, []);
   return (
     <div className="parent">
       <HeaderPage />
