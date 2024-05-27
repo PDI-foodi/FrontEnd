9d7337dc6778b4c06234ff5fc171956469c17174
lemonticsoul <sjho714@naver.com>
2024-05-27 10:23:10 +0900

    ✨feat:etc,flour 기능 추가

--- frontend/src/components/sort/sort.jsx
+++ frontend/src/components/sort/sort.jsx
@@ -7,7 +7,7 @@ export default function Sort() {
 
 
     const all=async ()=>{
-      const allitems=await axios.get("/sort");
+      const allitems=await axios.get("http://localhost:5000/sort");
       setShow(true)
       setData(allitems.data)
     }
@@ -17,32 +17,42 @@ export default function Sort() {
     }
     
     const western=async ()=>{
-      const western=await axios.get("/sort/western")
+      const western=await axios.get("http://localhost:5000/sort/western")
       setShow(true)
       setData(western.data)
     }
     
     const korea=async ()=>{
-      const korea=await axios.get("/sort/korea")
+      const korea=await axios.get("http://localhost:5000/sort/korea")
       setShow(true)
       setData(korea.data)
     }
     
     const japanese=async()=>{
-      const japanese=await axios.get("/sort/japanese")
+      const japanese=await axios.get("http://localhost:5000/sort/japanese")
       setShow(true)
       setData(japanese.data)
     }
     
     const desert=async()=>{
-      const desert=await axios.get("/sort/desert")
+      const desert=await axios.get("http://localhost:5000/sort/desert")
       setShow(true)
       setData(desert.data)
     }
     const fastfood=async ()=>{
-      const fastfood=await axios.get("/sort/fastfood")
+      const fastfood=await axios.get("http://localhost:5000/sort/fast-food")
       setShow(true)
       setData(fastfood.data)
+    }
+    const flour=async ()=>{
+      const flour=await axios.get("http://localhost:5000/sort/flour")
+      setShow(true)
+      setData(flour.data)
+    }
+    const etc=async ()=>{
+      const etc=await axios.get("http://localhost:5000/sort/etc")
+      setShow(true)
+      setData(etc.data)
     }
    
     
@@ -55,12 +65,16 @@ export default function Sort() {
                     <div onClick={korea}>한식</div>
                     <div onClick={japanese}>일식</div>
                     <div onClick={desert}>디저트</div>
+                    <div onClick={flour}>분식</div>
+                    <div onClick={etc}>기타</div>
                     <div onClick={fastfood}>패스트푸드</div>
       </div>
       {
         setShow &&(
           <div>
-
+            {
+              data.length
+            }
             {data.map((elem,index)=>(
               <div key={index+1} > 
               <img src={elem.imglink}></img>
