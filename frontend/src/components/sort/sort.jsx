c79582c813d2831a70ac3da7d70406a1668c258a
lemonticsoul <sjho714@naver.com>
2024-05-27 11:19:24 +0900

    ✨feat:푸드 랜덤함수 추가

--- frontend/src/components/sort/sort.jsx
+++ frontend/src/components/sort/sort.jsx
@@ -4,56 +4,92 @@ export default function Sort() {
 
     const [data, setData] = useState([]);
     const [show, setShow] = useState(false);
+    const [randomindex,setrandomindex]=useState([]);
+
+    const random =(length)=>{
+      return parseInt(Math.random()*length)
+    }
+    const limitedRandomNumbers=(data)=>{
+      const randomNumbers=[];
+      for (let i=0;i<=5;i++){
+        randomNumbers.push(random(data.length));
+      }
+      return randomNumbers;
+    }
+
+    const full=(data)=>{
+      if (data.length>=5){
+        const random=limitedRandomNumbers(data);
+        const new_data = data.filter((elem, index) => random.includes(index));
+        setData(new_data)
+      }
+      else{
+        setData(data);
+      }
+    }
 
 
     const all=async ()=>{
       const allitems=await axios.get("http://localhost:5000/sort");
       setShow(true)
-      setData(allitems.data)
+      const temp=allitems.data;
+      full(temp);
     }
 
     const jjim=()=>{
+
+      
+
+      
 
     }
     
     const western=async ()=>{
       const western=await axios.get("http://localhost:5000/sort/western")
       setShow(true)
-      setData(western.data)
+      const temp=western.data;
+      full(temp);
     }
     
     const korea=async ()=>{
       const korea=await axios.get("http://localhost:5000/sort/korea")
       setShow(true)
-      setData(korea.data)
+      const temp=korea.data;
+      full(temp);
     }
     
     const japanese=async()=>{
       const japanese=await axios.get("http://localhost:5000/sort/japanese")
       setShow(true)
-      setData(japanese.data)
+      const temp=japanese.data;
+      full(temp);
     }
     
     const desert=async()=>{
       const desert=await axios.get("http://localhost:5000/sort/desert")
       setShow(true)
-      setData(desert.data)
+      const temp=desert.data;
+      full(temp);
     }
     const fastfood=async ()=>{
       const fastfood=await axios.get("http://localhost:5000/sort/fast-food")
       setShow(true)
-      setData(fastfood.data)
+      const temp=fastfood.data;
+      full(temp);
     }
     const flour=async ()=>{
       const flour=await axios.get("http://localhost:5000/sort/flour")
       setShow(true)
-      setData(flour.data)
+      const temp=flour.data;
+      full(temp);
     }
     const etc=async ()=>{
       const etc=await axios.get("http://localhost:5000/sort/etc")
       setShow(true)
-      setData(etc.data)
+      const temp=etc.data;
+      full(temp);
     }
+    
    
     
   return (
@@ -72,9 +108,6 @@ export default function Sort() {
       {
         setShow &&(
           <div>
-            {
-              data.length
-            }
             {data.map((elem,index)=>(
               <div key={index+1} > 
               <img src={elem.imglink}></img>
