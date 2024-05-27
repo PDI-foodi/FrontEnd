70ce10d7b5d0dcc3954edaeb988227ab726156eb
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-05-27 12:50:38 +0900

    ✨feat: left page에 img 구현중

--- frontend/src/routes/main/detail/datailPage.left.jsx
+++ frontend/src/routes/main/detail/datailPage.left.jsx
@@ -1,4 +1,5 @@
 import "./detailPage.left.css";
+import StarRatings from "react-star-ratings";
 
 const DetailPageLeft = (props) => {
   console.log(props.data.imglink);
@@ -7,6 +8,17 @@ const DetailPageLeft = (props) => {
       <section>
         <div>
           <img src={props.imglink} alt="맛집 사진" />
+          <img
+            src={props.data.imglink}
+            alt="맛집 사진"
+            className="detail_food_img"
+          />
+          <img
+            src={props.data.imglink}
+            alt="맛집 사진"
+            className="detail_food_img"
+          />
+        </div>
         </div>
         <div></div>
       </section>
