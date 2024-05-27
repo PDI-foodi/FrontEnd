6684e9f963985b512ddf12c244202095f83daeb8
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-05-27 16:21:47 +0900

    ✨feat: 부트스트랩 환경 설정 완료

--- frontend/src/routes/main/detail/detailPage.review.jsx
+++ frontend/src/routes/main/detail/detailPage.review.jsx
@@ -1,11 +1,16 @@
 import "./detailPage.review.css";
+import Button from "react-bootstrap/Button";
 
 const detailReviewPage = () => {
+  const onClickAddReview = () => {};
   return (
     <section className="comment_div">
+      <Button />
       <div className="comment_header">
         <span className="comment_title">댓글</span>
-        <button className="comment_add_btn">리뷰 작성하기</button>
+        <button className="comment_add_btn" onClick={onClickAddReview}>
+          리뷰 작성하기
+        </button>
       </div>
       <div className="review_item"></div>
       <div className="review_item"></div>
