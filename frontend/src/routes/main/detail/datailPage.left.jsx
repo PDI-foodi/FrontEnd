52844e9f7c6362b69217b050a5cb9d5f06a2be9f
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-05-27 12:50:57 +0900

    ✨feat: 별정 라이브러리 사용 및 구현 완료

--- frontend/src/routes/main/detail/datailPage.left.jsx
+++ frontend/src/routes/main/detail/datailPage.left.jsx
@@ -2,12 +2,11 @@ import "./detailPage.left.css";
 import StarRatings from "react-star-ratings";
 
 const DetailPageLeft = (props) => {
-  console.log(props.data.imglink);
+  console.log(props.data);
   return (
     <div className="detail_item">
       <section>
         <div>
-          <img src={props.imglink} alt="맛집 사진" />
           <img
             src={props.data.imglink}
             alt="맛집 사진"
@@ -19,8 +18,22 @@ const DetailPageLeft = (props) => {
             className="detail_food_img"
           />
         </div>
+        <div className="food_info">
+          <h1>{props.data.name}</h1>
+          <span className="food_category">{props.data.category}</span>
+          <div className="food_star">
+            <StarRatings
+              rating={props.data.rate}
+              starRatedColor="gold"
+              numberOfStars={5}
+              name="rating"
+              className="food_star_item"
+              starDimension="22px" // 별의 크기를 지정합니다.
+              starSpacing="2px" // 별 간의 간격을 지정합니다.
+            />
+            <span className="star_score">{props.data.rate}점</span>
+          </div>
         </div>
-        <div></div>
       </section>
     </div>
   );
