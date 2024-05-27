4836b4af2b242039a188adbdfa19e20bda2e0748
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-05-27 11:25:07 +0900

    🎨design: 폰트 추가

--- frontend/src/component/Card.js
+++ frontend/src/component/Card.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+import { PiHeart, PiHeartFill } from "react-icons/pi";
+import { useNavigate } from "react-router-dom";
 import Card from "react-bootstrap/Card";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./Card.css";
 
-function Cards({ img, name, rate, rank, category }) {
+function Cards({ id, img, name, rate, rank, category }) {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+  const navigate = useNavigate();
+
   const renderStars = (rating) => {
     const stars = [];
     const fullStars = Math.floor(rating);
@@ -25,14 +30,33 @@ function Cards({ img, name, rate, rank, category }) {
     return stars;
   };
 
+  const handleCardClick = () => {
+    navigate(`/main/${id}`);
+  };
+
   return (
     <Card className="responsive-card">
-      <div className="rank-badge">{rank}</div>
+      {/* <div className="rank-badge">{rank}</div> */}
       <div className="card-img-container">
-        <img src={img} className="card-img" alt={name} />
-        <div className="card-img-gradient"></div>
+        <img src={img} className="card-img" alt={name} onClick={handleCardClick}/>
+        <div className="card-img-gradient" onClick={handleCardClick}></div>
       </div>
-      <Card.Body className="card-body">
+      {isHovered || isClicked ? (
+        <PiHeartFill
+          className="heartfill-badge"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          onClick={() => setIsClicked(!isClicked)}
+        />
+      ) : (
+        <PiHeart
+          className="heart-badge"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+          onClick={() => setIsClicked(!isClicked)}
+        />
+      )}
+      <Card.Body className="card-body" onClick={handleCardClick}>
         <Card.Title className="card-title">{name}</Card.Title>
         <Card.Text className="card-category">{category}</Card.Text>
         <Card.Text className="card-rating">{renderStars(rate)}</Card.Text>
