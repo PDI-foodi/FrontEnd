c4bb1def334bee7f2babec191270344b7309f845
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-05-28 16:50:41 +0900

    ✨ feat :  네이버 지도 연동 완료

--- frontend/src/routes/main/detail/detailPage.right.jsx
+++ frontend/src/routes/main/detail/detailPage.right.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
 import "./detailPage.right.css";
 import NaverMap from "./naverMap";
+import axios from "axios";
+import RightFoodItem from "./right.foodItem";
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6InlzeTA2MDUzIiwibmlja25hbWUiOiLsnbTsnqzsnbgiLCJpYXQiOjE3MTY3NzQ1MjUsImV4cCI6MTcxNzAzMzcyNX0.2XG3o1SmC8yBptHP3SBZWlPTQ_w_wupaaHBTgvBq-GU";
 
 const DetailPageRight = (props) => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get("/sort", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setData(res.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="detail_right_item">
       <NaverMap data={props.data} />
+      <div className="right_choice_text_div">
+        <span className="right_choice_text">다른 식당도 있어요</span>
+      </div>
+
+      <div className="right_food_list">
+        {data.map((e, i) => {
+          return <RightFoodItem data={e} key={i} />;
+        })}
+      </div>
     </div>
   );
 };
