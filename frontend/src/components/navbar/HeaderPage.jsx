fd5034797cd7bcdef8ff98035ebeb738b16c347b
YangJinHyeok <101380919+YangJinHyeok@users.noreply.github.com>
2024-05-29 10:59:00 +0900

    🐛fix: 오타 수정

--- frontend/src/components/navbar/HeaderPage.jsx
+++ frontend/src/components/navbar/HeaderPage.jsx
@@ -36,7 +36,7 @@ const HeaderPage = () => {
           onClick={handleCardClick}
         />
         <Button onClick={()=>navigate("/main/random")}>오늘 뭐 먹지?</Button>
-        <span className="user_info">환영하니다 {nickname}님</span>
+        <span className="user_info">환영합니다 {nickname}님</span>
         
       </div>
       <div className="line"></div>
