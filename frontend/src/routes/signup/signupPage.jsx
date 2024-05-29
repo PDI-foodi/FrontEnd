f6fb2ad34660f1d0d03d3747405e46f2baf9b3ac
lemonticsoul <127959482+lemonticsoul@users.noreply.github.com>
2024-05-29 14:55:59 +0900

    Revert "회원가입 페이지 구현"

--- frontend/src/routes/signup/signupPage.jsx
+++ frontend/src/routes/signup/signupPage.jsx
@@ -1,14 +1,4 @@
-import HeaderPage from "../../components/navbar/Signup-Header";
-import FooterPage from "../../components/footer/FooterPage";
-import Signup from "../../components/signup/Signup";
-
 const SignupPage = () => {
-  return (
-    <div>
-      <HeaderPage />
-      <Signup />
-      <FooterPage />
-    </div>
-  )
+  return <div>signupPage</div>;
 };
 export default SignupPage;
