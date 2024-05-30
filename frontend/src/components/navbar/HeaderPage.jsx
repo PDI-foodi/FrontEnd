e687bf0872945bb1a22b711dd1578792304ce950
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-05-30 16:04:04 +0900

    🎨 design : 테스트 줄 내림

--- frontend/src/components/navbar/HeaderPage.jsx
+++ frontend/src/components/navbar/HeaderPage.jsx
@@ -1,13 +1,12 @@
 import "./HeaderPage.css";
 import { useNavigate } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 const HeaderPage = () => {
   const navigate = useNavigate();
   const [nickname, setNickname] = useState([]);
-
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,7 +21,6 @@ const HeaderPage = () => {
     fetchData();
   }, []);
 
-
   const handleCardClick = () => {
     navigate(`/main`);
   };
@@ -36,10 +34,17 @@ const HeaderPage = () => {
           onClick={handleCardClick}
         />
         <div className="headerDiv">
-          <Button onClick={()=>navigate("/main/random")} className="headerbutton">오늘 뭐 먹지?</Button>
-          <span className="user_info">환영합니다 {nickname}님</span>
+          <Button
+            onClick={() => navigate("/main/random")}
+            className="headerbutton"
+          >
+            오늘 뭐 먹지?
+          </Button>
+          <span className="user_info">
+            환영합니다 <br />
+            {nickname}님
+          </span>
         </div>
-        
       </div>
       <div className="line"></div>
     </>
